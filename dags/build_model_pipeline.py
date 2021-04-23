23ad09116c6ce00ca54c932d6b4e31e919dcffc9
blipp <6Fqshfzlx3hpKgZEWZbu>
2021-04-22 21:38:14 -0400

    updating the dags to trigger the spark jobs

--- dags/build_model_pipeline.py
+++ dags/build_model_pipeline.py
@@ -1,12 +1,15 @@
+from datetime import datetime
+import os
+
 from airflow import DAG
 from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
-
 
 with DAG(
         dag_id='build_model_pipeline',
         schedule_interval=None,
-        start_date=None
+        start_date=datetime(1981, 1, 1)
 ) as dag:
+
     preprocess_data = DatabricksRunNowOperator(task_id='preprocess_data',
                                                job_id=3)
     build_model = DatabricksRunNowOperator(task_id='build_model',
--- dags/search_model_pipeline.py
+++ dags/search_model_pipeline.py
@@ -1,11 +1,15 @@
+from datetime import datetime
+import os
+
 from airflow import DAG
 from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
 
 with DAG(
         dag_id='search_model_pipeline',
         schedule_interval=None,
-        start_date=None
+        start_date=datetime(1981, 1, 1)
 ) as dag:
+
     preprocess_data = DatabricksRunNowOperator(task_id='preprocess_data',
                                                job_id=3)
     search_model = DatabricksRunNowOperator(task_id='search_model',
