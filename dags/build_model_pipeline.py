4e063c083fe3f8cc67b9682aab82d68ec6d451b4
blipp <bclipp770@gmail.com>
2021-04-28 10:44:13 -0400

    switching to newsub, and updating with new file location

--- dags/build_model_pipeline.py
+++ dags/build_model_pipeline.py
@@ -8,16 +8,22 @@ with DAG(
         schedule_interval=None,
         start_date=datetime(1981, 1, 1)
 ) as dag:
-    preprocess_data = DatabricksRunNowOperator(task_id="preprocess_data",
-                                               job_id=3)
+    # preprocess_data = DatabricksRunNowOperator(task_id="preprocess_data",
+    #                                           job_id=3)
     # build_model = DatabricksRunNowOperator(task_id="build_model_old",
     #                                      job_id=4)
+    
+    preprocess_data = DatabricksSubmitRunOperator(
+        task_id="preprocess_data",
+        spark_python_task={"python_file": "dbfs:/datalake/code/preprocessing/app/__main__.py",
+                           "parameters": ""},
+        existing_cluster_id="0421-172042-scats73"
+    )
 
     build_model = DatabricksSubmitRunOperator(
         task_id="build_model",
-        spark_python_task={"python_file": "dbfs:/user/bclipp770@yandex.com/MLOps_model_python/__main__.py",
+        spark_python_task={"python_file": "dbfs:/datalake/code/model/app/__main__.py",
                            "parameters": ""},
-        # {"python file path and parameters to run the python file with"},
         existing_cluster_id="0421-172042-scats73"
     )
 
