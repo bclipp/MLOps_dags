3833d05022b016feb16476cf56b38d01bcbfbeb3
blipp <bclipp770@gmail.com>
2021-04-27 22:42:23 -0400

    adding submit and working

--- dags/build_model_pipeline.py
+++ dags/build_model_pipeline.py
@@ -1,23 +1,24 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator ,DatabricksSubmitRunOperator
+from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator, DatabricksSubmitRunOperator
 
 with DAG(
-        dag_id='build_model_pipeline',
+        dag_id="build_model_pipeline",
         schedule_interval=None,
         start_date=datetime(1981, 1, 1)
 ) as dag:
-    preprocess_data = DatabricksRunNowOperator(task_id='preprocess_data',
+    preprocess_data = DatabricksRunNowOperator(task_id="preprocess_data",
                                                job_id=3)
-    build_model = DatabricksRunNowOperator(task_id='build_model',
-                                           job_id=4)
+    # build_model = DatabricksRunNowOperator(task_id="build_model_old",
+    #                                      job_id=4)
 
     build_model = DatabricksSubmitRunOperator(
-        task_id='build_model',
-        spark_python_task={"python file path and parameters to run the python file with"},
-        existing_cluster_id="tiny",
+        task_id="build_model",
+        spark_python_task={"python_file": "dbfs:/user/bclipp770@yandex.com/MLOps_model_python/__main__.py",
+                           "parameters": ""},
+        # {"python file path and parameters to run the python file with"},
+        existing_cluster_id="0421-172042-scats73"
     )
-    # pass Hyper
 
     preprocess_data >> build_model
