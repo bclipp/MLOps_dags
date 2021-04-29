539d6e66e421b0abd0a06161bb246259caf5b697
blipp <bclipp770@gmail.com>
2021-04-28 23:03:31 -0400

    playing with passing uuid

--- dags/build_model_pipeline.py
+++ dags/build_model_pipeline.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import uuid
 from airflow import DAG
 from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
 
@@ -12,18 +12,18 @@ with DAG(
     #                                           job_id=3)
     # build_model = DatabricksRunNowOperator(task_id="build_model_old",
     #                                      job_id=4)
-
+    uid = str(uuid.uuid1()).replace('-', '')
     preprocess_data = DatabricksSubmitRunOperator(
         task_id="preprocess_data",
         spark_python_task={"python_file": "dbfs:/datalake/code/preprocessing/__main__.py",
-                           "parameters": ""},
+                           "parameters": f"{uid}"},
         existing_cluster_id="0421-172042-scats73"
     )
 
     build_model = DatabricksSubmitRunOperator(
         task_id="build_model",
         spark_python_task={"python_file": "dbfs:/datalake/code/model/__main__.py",
-                           "parameters": ""},
+                           "parameters": f"{uid}"},
         existing_cluster_id="0421-172042-scats73"
     )
 
