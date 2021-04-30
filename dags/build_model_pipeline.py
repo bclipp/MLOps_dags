43c4f6f7dee9c6b903a4439a63d3fc8c432a68f3
blipp <bclipp770@gmail.com>
2021-04-29 23:08:07 -0400

    added uid passing correctly, using date

--- dags/build_model_pipeline.py
+++ dags/build_model_pipeline.py
@@ -12,19 +12,23 @@ with DAG(
     #                                           job_id=3)
     # build_model = DatabricksRunNowOperator(task_id="build_model_old",
     #                                      job_id=4)
-    uid = str(uuid.uuid1()).replace('-', '')
+    # uid = str(uuid.uuid1()).replace('-', '')
+    timestamp = datetime.now()
+    uid = timestamp.strftime("%b%d%Y")
+    print("UID: " + uid)
+    cluster_id = "0428-143146-wig801"
     preprocess_data = DatabricksSubmitRunOperator(
         task_id="preprocess_data",
         spark_python_task={"python_file": "dbfs:/datalake/code/preprocessing/__main__.py",
                            "parameters": f"{uid}"},
-        existing_cluster_id="0421-172042-scats73"
+        existing_cluster_id=cluster_id
     )
 
     build_model = DatabricksSubmitRunOperator(
         task_id="build_model",
         spark_python_task={"python_file": "dbfs:/datalake/code/model/__main__.py",
                            "parameters": f"{uid}"},
-        existing_cluster_id="0421-172042-scats73"
+        existing_cluster_id=cluster_id
     )
 
     preprocess_data >> build_model
