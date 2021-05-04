46baa66a829e09f8bd4e9b2ffd60bf7d854746c3
blipp <bclipp770@gmail.com>
2021-05-04 08:44:10 -0400

    adding cluster dag for search

---
+++ dags/cluster_search_model_pipeline.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
+
+with DAG(
+        dag_id='search_model_pipeline',
+        schedule_interval=None,
+        start_date=datetime(1981, 1, 1)
+) as dag:
+    preprocess_data = DatabricksSubmitRunOperator(
+        task_id="preprocess_data",
+        spark_python_task={"python_file": "dbfs:/datalake/code/preprocessing/app/__main__.py",
+                           "parameters": ""},
+        existing_cluster_id="0421-172042-scats73"
+    )
+
+    search_model = DatabricksSubmitRunOperator(
+        task_id="search_model",
+        spark_python_task={"python_file": "dbfs:/datalake/code/cluster_search/app/__main__.py",
+                           "parameters": ""},
+        existing_cluster_id="0421-172042-scats73"
+    )
+
+    preprocess_data >> search_model
