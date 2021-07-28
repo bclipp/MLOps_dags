6c4c3546c3e10ea9540b94dec7ddfda005b3bab5
Brian Lipp <bclipp770@gmail.com>
2021-07-28 14:07:38 -0400

    Update search_model_pipeline.py
    adding uid to dag

--- dags/search_model_pipeline.py
+++ dags/search_model_pipeline.py
@@ -11,17 +11,19 @@ with DAG(
         schedule_interval=None,
         start_date=datetime(1981, 1, 1)
 ) as dag:
+    timestamp = datetime.now()
+    uid = timestamp.strftime("%b%d%Y")
     preprocess_data = DatabricksSubmitRunOperator(
         task_id="preprocess_data",
         spark_python_task={"python_file": "dbfs:/datalake/code/preprocessing/__main__.py",
-                           "parameters": ""},
+                           "parameters": f"{uid}"},
         existing_cluster_id="0505-024656-wont404"
     )
 
     search_model = DatabricksSubmitRunOperator(
         task_id="search_model",
         spark_python_task={"python_file": "dbfs:/datalake/code/search/__main__.py",
-                           "parameters": ""},
+                           "parameters": f"{uid}"},
         existing_cluster_id="0505-024656-wont404"
     )
 
